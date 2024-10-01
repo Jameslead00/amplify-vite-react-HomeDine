3b611e80918708a5beeeafb14bdebd1a46f3e4cd
Leadbjam <leadbjam@students.zhaw.ch>
2024-10-01 21:41:49 +0200

    added s3 buckets for profile pics and service pics

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,22 @@
 import { defineStorage } from '@aws-amplify/backend';
 
-export const storage = defineStorage({
-  name: 'HomeDineBucket'
+export const profilePicBucket = defineStorage({
+  name: 'profilePicBucket',
+  isDefault: true,
+  access: (allow) => ({
+    'profile-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+  })
 });
+
+export const servicePicBucket = defineStorage({
+  name: 'servicePicBucket',
+  access: (allow) => ({
+    'service-pictures/{entity_id}/*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+  })
+})
